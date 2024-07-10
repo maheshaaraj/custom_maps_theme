c4935a032491aaecd6eb68c3a6a64ee4c9a6f245
mahesharaj <99277198+maheshaaraj@users.noreply.github.com>
2024-07-10 22:26:30 +0530

    map

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log("working");
+
+var map = L.map('map').setView([19.071457, 72.884674], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// L.marker([51.5, -0.09]).addTo(map)
+//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//     .openPopup();
+
+// var marker = L.marker([25.5, -0.11]).addTo(map)
+//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+//     .openPopup();
+
+var popup = L.popup();
+
+    function onMapClick(e) {
+        popup
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(map);
+    }
+    
+    map.on('click', onMapClick);
+
